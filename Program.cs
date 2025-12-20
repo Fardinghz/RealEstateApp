ef93f4a867e3d077c145e8a7e8fdb7aed97ea2cc
unknown <fardinghaznavi@gmail.com>
2025-12-20 13:06:07 +0330

    new

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ class Program
     static void Main(string[] args)
     {
         Property first = new Villa(22 , "fardin" , 911 ,200 , 100 , 1399 , 2 , "sangar azadi" , true);
-        
+        System.Console.WriteLine(first.Getinfo());
         
 
 
